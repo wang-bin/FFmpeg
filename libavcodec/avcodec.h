e3aca538f8808be4d4e897d77355d21eaa30acbb
wang-bin <wbsecg1@gmail.com>
2017-11-09 14:56:53 +0800

    avcodec: add AV_HWACCEL_FLAG_ALLOW_SOFTWARE
    a hw decoder may have software or hybrid implementation, for example videotoolbox hevc.
    the performance may be better than ffmpeg sw decoder.

--- libavcodec/avcodec.h
+++ libavcodec/avcodec.h
@@ -2215,6 +2215,10 @@ typedef struct AVHWAccel {
 #define AV_HWACCEL_FLAG_UNSAFE_OUTPUT (1 << 3)
 
 /**
+ * Hardware acceleration can use it's software implementation.
+ */
+#define AV_HWACCEL_FLAG_ALLOW_SOFTWARE (1 << 8)
+/**
  * @}
  */
 
