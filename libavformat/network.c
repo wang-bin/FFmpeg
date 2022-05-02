615d7f99e2dbc291c94700bb096ee20ccf13240e
wang-bin <wbsecg1@gmail.com>
2022-05-03 00:03:18 +0800

    wolfSSL - new TLS backend patch
    https://lists.ffmpeg.org/pipermail/ffmpeg-devel/2018-August/233802.html

--- libavformat/network.c
+++ libavformat/network.c
@@ -39,6 +39,9 @@ int ff_tls_init(void)
 #if CONFIG_GNUTLS
     ff_gnutls_init();
 #endif
+#if CONFIG_WOLFSSL
+    ff_wolfssl_init();
+#endif
 #endif
     return 0;
 }
@@ -48,6 +51,9 @@ void ff_tls_deinit(void)
 #if CONFIG_TLS_PROTOCOL
 #if CONFIG_GNUTLS
     ff_gnutls_deinit();
+#endif
+#if CONFIG_WOLFSSL
+    ff_wolfssl_deinit();
 #endif
 #endif
 }
