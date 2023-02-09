c28f67a144ed0f97dd69e152be671935ca29ff5f
wang-bin <wbsecg1@gmail.com>
2023-02-09 18:08:11 +0800

    avformat/mpegts: index only keyframes to ensure accurate seeks
    https://patchwork.ffmpeg.org/project/ffmpeg/patch/20190507032623.80375-1-ffmpeg@tmm1.net/

--- libavformat/mpegts.c
+++ libavformat/mpegts.c
@@ -3361,7 +3361,7 @@ static int64_t mpegts_get_dts(AVFormatContext *s, int stream_index,
             av_packet_free(&pkt);
             return AV_NOPTS_VALUE;
         }
-        if (pkt->dts != AV_NOPTS_VALUE && pkt->pos >= 0) {
+        if (pkt->dts != AV_NOPTS_VALUE && pkt->pos >= 0  && (pkt->flags & AV_PKT_FLAG_KEY)) {
             ff_reduce_index(s, pkt->stream_index);
             av_add_index_entry(s->streams[pkt->stream_index], pkt->pos, pkt->dts, 0, 0, AVINDEX_KEYFRAME /* FIXME keyframe? */);
             if (pkt->stream_index == stream_index && pkt->pos >= *ppos) {
