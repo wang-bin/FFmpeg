41955636ddf26d697921851baf26552aff35e22e
wang-bin <wbsecg1@gmail.com>
2024-12-31 18:49:16 +0800

    avutil/mem_internal: fix clang + vcrt build
    fix many sws link error if build with clang --target=x86_64-pc-windows-msvc + vcrt + winsdk, for example
    lld-link: error: undefined symbol: bFC
    >>> referenced by /home/runner/work/avbuild/avbuild/build_sdk-windows-desktop-x64-clang/src/libswscale/x86/swscale_template.c:383
    >>>               libswscale/x86/swscale.o:(yuv2rgb565_X_ar_mmxext)
    >>> referenced by /home/runner/work/avbuild/avbuild/build_sdk-windows-desktop-x64-clang/src/libswscale/x86/swscale_template.c:405
    >>>               libswscale/x86/swscale.o:(yuv2rgb565_X_mmxext)
    >>> referenced by /home/runner/work/avbuild/avbuild/build_sdk-windows-desktop-x64-clang/src/libswscale/x86/swscale_template.c:1273
    >>>               libswscale/x86/swscale.o:(yuv2rgb565_1_mmxext)
    >>> referenced 2 more times
    
    c11 only defines _Alignas, alignas requires c23, or include stdalign.h which defines alignas for c11. but stdalign.h does not always exist.
    for msvc, __STDC_VERSION__ macro is defined since vs16.7(2019) vc19.27. assume it's always defined by other compilers.
    _Alignas crashes for msvc build.

--- libavutil/mem_internal.h
+++ libavutil/mem_internal.h
@@ -24,7 +24,7 @@
 #include "config.h"
 
 #include <stdint.h>
-#ifndef _MSC_VER
+#if !defined(_MSC_VER) || defined(__clang__)
 #include <stdalign.h>
 #endif
 
@@ -80,7 +80,7 @@
     #define DECLARE_ALIGNED_T(n,t,v)    alignas(FFMIN(n, 16)) t v
     #define DECLARE_ASM_ALIGNED(n,t,v)  alignas(FFMIN(n, 16)) t av_used v
     #define DECLARE_ASM_CONST(n,t,v)    alignas(FFMIN(n, 16)) static const t av_used v
-#elif defined(_MSC_VER)
+#elif defined(_MSC_VER) && !defined(__clang__)
     #define DECLARE_ALIGNED_T(n,t,v)    __declspec(align(n)) t v
     #define DECLARE_ASM_ALIGNED(n,t,v)  __declspec(align(n)) t av_used v
     #define DECLARE_ASM_CONST(n,t,v)    __declspec(align(n)) static const t av_used v
